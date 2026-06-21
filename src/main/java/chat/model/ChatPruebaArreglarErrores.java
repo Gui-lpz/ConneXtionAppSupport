4b8b5887dc735e1d951dda29abb8a817d226b071
AdrielCaféMS <adrielms21@gmail.com>
2026-06-20 21:22:19 -0600

    US-15: Implementar sockets para chat

---
+++ src/main/java/chat/model/ChatPruebaArreglarErrores.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chat.model;
+
+/**
+ *
+ * @author adrie
+ */
+public class ChatPruebaArreglarErrores {
+    
+   /* SLAJHVVHASLAS
+            d
+            s
+                    sad
+                    asd
+                            asd
+                            sad
+                                    s
+                                    ad
+                                            as
+                                            das
+                                                    asd
+                                                    sad
+                                                            asd
+                                                            asd
+                                                                    asd
+                                                                    asd
+                                                                            asd
+                                                                            asd
+                                                                                    sad
+                                                                                    sad
+                                                                                            sad
+                                                                                            asd
+                                                                                                    asd
+                                                                                                    sad
+                                                                                                            asd
+                                                                                                            sad
+                                                                                                                    asd
+                                                                                                                    asd
+                                                                                                                            asd
+                                                                                                                            */
+}
